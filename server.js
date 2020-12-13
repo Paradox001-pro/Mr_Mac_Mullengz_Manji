7b26d904e1922bdfd430e6e577527c0c96b2ad68
Paradox001-pro <60643966+Paradox001-pro@users.noreply.github.com>
2020-12-14 00:32:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
-const expressLayouts = require('ejs');
+const bodyParser = require('body-parser');
+
+const expressLayouts = require('express-ejs-layouts');
 
 
 const app = express();
@@ -34,15 +36,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, '')))
 
+
 // EJS
-// app.use(expressLayouts);
+app.use(expressLayouts);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
 // BODYPARSER
-app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false}));
 
 
 app.use(session({
